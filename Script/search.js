8719a24bf013b38b509987b6f545a131c9aef287
mAzal-dev <muhd.azal@gmail.com>
2023-02-26 00:04:43 +0800

    Display states details in table(not completed)

--- Script/search.js
+++ Script/search.js
@@ -3,50 +3,6 @@ const list = document.createDocumentFragment();
 const myAPIUrl = "https://localhost:7249/api/getstate";
 //const myAPIUrl = "https://jsonplaceholder.typicode.com/users";
 
-
-// function searchState(){
-//     var state = document.getElementById('stateString').value;
-//     if(state){
-//         const myHeaders = new Headers({
-//             'Content-Type':'application/json'
-//         });
-
-//         console.log(myHeaders);
-
-//         const myRequest = new Request(myAPIUrl, {
-//             method: "GET",
-//             headers: myHeaders,
-//             mode: "cors",
-//             cache: "default",
-//         });
-//         console.log(myRequest);
-//         //Proceed to call API
-//         fetch(myRequest, {
-//             credentials:"include",
-//         })
-//             .then((response) => {
-//                 const contentType = response.headers.get("content-type");
-//                 if(!contentType || !contentType.includes("application/json"))
-//                 {
-//                     //console.log(contentType);
-//                     throw new TypeError("Opps, not JSON!");
-//                 }
-//                 console.log(response.json());
-//             })
-//             .catch(function(){
-//                 throw new TypeError("API Failed");
-//             })        
-//     }
-//     else{
-//         //Return input Null
-//         alert('Please Enter Value to Proceed');
-
-//     }
-    
-
-    
-// }
-
 function searchState(){
     fetch(myAPIUrl)
         .then((response) => {
@@ -54,7 +10,15 @@ function searchState(){
         })
         .then((data) => {
             let states = data;
-
             console.log(states);
-        })
+            length=data.data.length
+            var temp="";
+            for(i=0;i<length;i++){
+                temp+="<tr>";
+                temp+="<td>"+data.data[i].state+"</td>";
+                temp+="<td>"+data.data[i].capital+"</td>";
+            }
+            document.getElementById("tblStates").innerHTML=temp;
+        });
+        
 }
