d3dcbc22c77f7c2e10188cfdd127ecb90dd75501
mAzal-dev <muhd.azal@gmail.com>
2023-02-25 22:09:48 +0800

    Fix CORS error by adding mode:no-cors

--- Script/search.js
+++ Script/search.js
@@ -1,9 +1,27 @@
+const myAPIUrl = "https://localhost:7249/api/getstate";
+
 function searchState(){
     var state = document.getElementById('stateString').value;
     if(state){
+        const myHeaders = new Headers();
+
+        const myRequest = new Request(myAPIUrl, {
+            method: "GET",
+            headers: myHeaders,
+            mode: "no-cors",
+            cache: "default",
+        });
         //Proceed to call API
-        alert('Done!');
-        
+        fetch(myRequest)
+            .then((response) => {
+                const contentType = response.headers.get("content-type");
+                if(!contentType || !contentType.includes("application/json"))
+                {
+                    console.log(contentType);
+                    throw new TypeError("Opps, not JSON!");
+                }
+                console.log(response.json());
+            })        
     }
     else{
         //Return input Null
